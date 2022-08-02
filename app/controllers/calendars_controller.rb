8f8feebf13befcc36d2786dc1ffb5ff2f1a2e8e1
lemon0922 <ima.com0922@icloud.com>
2022-08-02 17:54:44 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -16,6 +16,7 @@ class CalendarsController < ApplicationController
 
   def plan_params
     params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
